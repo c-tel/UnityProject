36ebee504c4158b5a62162b65a2f0eb14999c70a
c-tel <rorex555@gmail.com>
2018-06-02 18:40:01 +0300

    Lab02

---
+++ Assets/Scripts/DeathHere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		HeroRabbit rabit = collider.GetComponent<HeroRabbit> ();
+		if(rabit != null) 
+			LevelController.current.onRabbitDeath(rabit);
+}
+}
--- Assets/Scripts/HeroRabbit.cs
+++ Assets/Scripts/HeroRabbit.cs
@@ -6,16 +6,28 @@ public class HeroRabbit : MonoBehaviour {
 	public float speed = 1;
 	float value;
 	Rigidbody2D myBody = null;
+	
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
+	
 	// Use this for initialization
 	void Start  () {
 		value = Input.GetAxis ("Horizontal");
 		myBody = this.GetComponent<Rigidbody2D> ();
+		LevelController.current.setStartPosition (transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Animator animator = GetComponent<Animator> ();
+		animator.SetBool ("run", Mathf.Abs(value) > 0);
+		animator.SetBool ("jump", !this.isGrounded);
+	
 	}
+	
 	void FixedUpdate () {
 		value = Input.GetAxis ("Horizontal");
 		if (Mathf.Abs (value) > 0) {
@@ -30,5 +42,28 @@ public class HeroRabbit : MonoBehaviour {
 		} else if(value > 0) {
 			sr.flipX = false;
 		}
+		Vector3 fr = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		RaycastHit2D hit = Physics2D.Linecast(fr, to, layer_id);
+		isGrounded = hit;
+		
+		if (Input.GetButtonDown("Jump") && isGrounded)
+			this.JumpActive = true;
+		
+		if(this.JumpActive) {
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+		
 	}
 }
